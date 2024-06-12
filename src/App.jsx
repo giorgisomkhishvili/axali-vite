7a400d343064712f2d51e00523f6689927a966a9
giorgisomkhishvili <somkhishvilig22@gmail.com>
2024-06-12 11:46:13 +0400

    p change
    axali cvlileba!!

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p style={{ color: 'red', fontSize: '50px' }}>
-          yes baby a'm here,daje icvleba
+          yes baby a'm here,daje icvleba,axali cvlileba!!
         </p>
       </div>
       <p className="read-the-docs">
